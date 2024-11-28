20dd30dca2913b7137bb310bc2b4c4da6635aeb5
yoonseo8167 <yoonseo8167@sookmyung.ac.kr>
2024-11-28 21:14:36 +0900

    프로필 CONTENT 수정

--- app/src/main/java/com/example/hu_project/ProfileFragment.kt
+++ app/src/main/java/com/example/hu_project/ProfileFragment.kt
@@ -5,12 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hu_project.adapter.HorizontalPostAdapter
 import com.example.hu_project.databinding.FragmentProfileBinding
+import com.example.hu_project.model.main_data
+import com.google.android.material.tabs.TabLayout
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var adapter: HorizontalPostAdapter // 게시글 recyclerView 어댑터
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +25,36 @@ class ProfileFragment : Fragment() {
     ): View? {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 더미 데이터
+        val playlistData = createDummyPosts("Playlist")
+        val eatData = createDummyPosts("Eat")
+        val lookData = createDummyPosts("Look")
+        val placeData = createDummyPosts("Place")
+
+        // RecyclerView 연결
+        setupHorizontalRecyclerView(binding.profilePlaylistRecyclerView, playlistData)
+        setupHorizontalRecyclerView(binding.profileEatRecyclerView, eatData)
+        setupHorizontalRecyclerView(binding.profileLookRecyclerView, lookData)
+        setupHorizontalRecyclerView(binding.profilePlaceRecyclerView, placeData)
+    }
+
+    private fun setupHorizontalRecyclerView(recyclerView: RecyclerView, posts: List<main_data.Post>) {
+        recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.adapter = HorizontalPostAdapter(posts)
+    }
+
+    private fun createDummyPosts(category: String): List<main_data.Post> {
+        return List(5) { index ->
+            main_data.Post(
+                title = "Post Title $index",
+                imageUrl = "@drawable/whale"
+            )
+        }
     }
 
     override fun onDestroyView() {
